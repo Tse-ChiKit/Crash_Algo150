ad63de49b1f7e6fc12c6b18aea58c079a05721fe
Eric Xie <397706572@qq.com>
2026-08-23 20:21:35 +0800

    Add Day 6 Rotate Array solution

---
+++ 0189-rotate-array/solution.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def reverse(self, nums: List[int], left: int, right: int) -> None:
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+
+    def rotate(self, nums: List[int], k: int) -> None:
+        k = k % len(nums)
+
+        self.reverse(nums, 0, len(nums) - 1)
+        self.reverse(nums, 0, k - 1)
+        self.reverse(nums, k, len(nums) - 1)
